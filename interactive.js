2bc555e27d51daccd1dac85cb0be6d93cc036168
immanual19 <immanuelsarker1@gmail.com>
2021-01-29 19:54:37 +0600

    fixed

--- interactive.js
+++ interactive.js
@@ -1,8 +1,9 @@
+var flag=0;
 document.getElementById("addition").addEventListener("click",function(){
 
     const result=document.getElementById("res").innerText;
     const lastEntry=result[result.length-1];
-    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/"))
+    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/") &&flag!=1)
     {
         document.getElementById("res").innerText=result.concat("+");
     }
@@ -12,7 +13,7 @@ document.getElementById("subtraction").addEventListener("click",function(){
 
     const result=document.getElementById("res").innerText;
     const lastEntry=result[result.length-1];
-    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/"))
+    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/") &&flag!=1)
     {
         document.getElementById("res").innerText=result.concat("-");
     }
@@ -22,7 +23,7 @@ document.getElementById("multiplication").addEventListener("click",function(){
 
     const result=document.getElementById("res").innerText;
     const lastEntry=result[result.length-1];
-    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/"))
+    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/") &&flag!=1)
     {
         document.getElementById("res").innerText=result.concat("*");
     }
@@ -32,7 +33,7 @@ document.getElementById("division").addEventListener("click",function(){
 
     const result=document.getElementById("res").innerText;
     const lastEntry=result[result.length-1];
-    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/"))
+    if(result!="" && (lastEntry!="+" && lastEntry!="-" && lastEntry!="*" && lastEntry!="/") &&flag!=1)
     {
         document.getElementById("res").innerText=result.concat("/");
     }
@@ -40,40 +41,113 @@ document.getElementById("division").addEventListener("click",function(){
 
 document.getElementById("one").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("1");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+1;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("1");
+    }
+    
 })
 document.getElementById("two").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("2");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+2;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("2");
+    }
 })
 document.getElementById("three").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("3");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+3;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("3");
+    }
 })
 
 document.getElementById("four").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("4");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+4;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("4");
+    }
 })
 document.getElementById("five").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("5");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+5;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("5");
+    }
 })
 document.getElementById("six").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("6");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+6;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("6");
+    }
 })
 document.getElementById("seven").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("7");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+7;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("7");
+    }
 })
 document.getElementById("eight").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("8");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+8;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("8");
+    }
 })
 document.getElementById("nine").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
-    document.getElementById("res").innerText=result.concat("9");
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+9;
+        flag=0;
+    }
+    else
+    {
+        document.getElementById("res").innerText=result.concat("9");
+    }
 })
 
 document.getElementById("zero").addEventListener("click",function(){
@@ -86,7 +160,15 @@ document.getElementById("zero").addEventListener("click",function(){
 
 document.getElementById("dot").addEventListener("click",function(){
     const result=document.getElementById("res").innerText;
+    if(flag==1)
+    {
+        document.getElementById("res").innerText=""+".";
+        flag=0;
+    }
+    else
+    {
         document.getElementById("res").innerText=result.concat(".");
+    }
 })
 
 document.getElementById("deleteAll").addEventListener("click",function(){
@@ -100,6 +182,7 @@ document.getElementById("equal").addEventListener("click",function(){
     {
        const finalResult=parseFloat(evaluate(result)).toFixed(2);
        document.getElementById("res").innerText=finalResult;
+       flag=1;
     }
 })
 function evaluate(str) {
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-function evil(fn) {
-    return new Function('return ' + fn)();
-  }
-  console.log( evil('12/5*9+9.4*2') );
+function testing()
+{
+  globalThis samp=10;
+}
+console.log(samp);
